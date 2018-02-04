7a8943dcf979d5daea3bcd402daf6ed67581f929
Kazuo Yagi <kazuo.yagi@gmail.com>
2018-02-04 11:09:17 +0900

    Added datetime and newline for my preference

--- agnoster.bash
+++ agnoster.bash
@@ -417,7 +417,7 @@ set_bash_prompt() {
 
     # uncomment below to use right prompt
     #     PS1='\[$(tput sc; printf "%*s" $COLUMNS "$PRIGHT"; tput rc)\]'$PR
-    PS1=$PR
+    PS1="${PR} \d \t \n$ "
 }
 
 PROMPT_COMMAND=set_bash_prompt
